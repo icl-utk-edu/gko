d3f4874b6c7e3f9138e5422e2966bb5366849676
Piotr Luszczek <luszczek@icl.utk.edu>
2024-02-25 22:25:03 +0000

    Complete transferring help strings

--- gko/argvparse.py
+++ gko/argvparse.py
@@ -12,6 +12,7 @@ line argument parsing.
 
 
 import argparse
+import gko
 from .commands import dispatch
 
 
@@ -19,14 +20,15 @@ _CommonOpts = {
     "1": {
         "l": "--parameter",
         "k": {
-            "help": "parameter number start at 1 that is predicted by the command'",
+            "help": "parameter number start at 1 that is predicted by the "
+            "command",
             "choices": (1,),  # this will be replaced by arg parsing below
         },
     },
     "c": {
         "l": "--candidates",
         "k": {
-            "help": "filename for the samples updated by the command'",
+            "help": "filename for the samples updated by the command",
             "type": argparse.FileType("r"),
         },
     },
@@ -35,7 +37,7 @@ _CommonOpts = {
         "k": {
             "help": "fidelity to use for the command",
             "type": int,
-            "nargs": "+",
+            "nargs": argparse.ONE_OR_MORE,
         },
     },
     "g": {
@@ -55,8 +57,9 @@ _CommonOpts = {
     "l": {
         "l": "--level",
         "k": {
-            "help": "filename for the command's graph",
+            "help": "levels the command's graph",
             "type": int,
+            "nargs": argparse.ZERO_OR_MORE,
         },
     },
     "M": {
@@ -74,10 +77,12 @@ _CommonOpts = {
         },
     },
     "n": {
+        "s": "n",
         "l": "--sample-count",
         "k": {
-            "help": "number of samples",
+            "help": "sample count (at least 2 required)",
             "type": int,
+            "nargs": argparse.ONE_OR_MORE,
         },
     },
     "O": {
@@ -97,14 +102,14 @@ _CommonOpts = {
     "P": {
         "l": "--perf-candid",
         "k": {
-            "help": "filename for the performance candidates for the command'",
+            "help": "filename for the performance candidates for the command",
             "type": argparse.FileType("r"),
         },
     },
     "p": {
         "l": "--perf",
         "k": {
-            "help": "filename for the performance values for the command'",
+            "help": "filename for the performance values for the command",
             "type": argparse.FileType("r"),
         },
     },
@@ -118,7 +123,7 @@ _CommonOpts = {
     "s": {
         "l": "--samples",
         "k": {
-            "help": "filename for the samples updated by the command'",
+            "help": "filename for the samples updated by the command",
             "type": argparse.FileType("r"),
         },
     },
@@ -129,12 +134,14 @@ def parse(argv: [str]):
     main = argparse.ArgumentParser(
         prog=argv[0],
         description="GKO or Generative Kernel Optimization",
-        epilog="The project is currently under development and new commands are possible in the future.",
+        epilog="New command possible in the future.",
         prefix_chars="-",
         usage="%(prog)s <command> [<command>] [options]")
 
+    main.add_argument("-v", "--version", action="version",
+                      version="%(prog)s {}".format(gko.__version__))
     main.add_argument("-c", "--config", type=argparse.FileType("r"),
-        help="path to main configuration file")
+                      help="path to main configuration file")
 
     command = main.add_subparsers(
         title="Actions",
@@ -144,14 +151,15 @@ def parse(argv: [str]):
 
     parsers = dict()
     for cmd, flgs, hlp in (
-         ("convert", "io", "convert in/out files between origin and noermalized data spaces"),
-         ("merge", "cflsPp", "sample the input space"),
-         ("model", "fgilOops", "model the input space with a learned model"),
-         ("plot", "cflMmops", "plot the samples and predictions"),
-         ("predict", "flmO4", "predict performance values in output space"),
-         ("search", "flMmSo", "search optimal values in the output space"),
-         ("sample", "fgilnOo", "sample the input space"),
-         ):
+        ("convert", "io", "convert in- or output between origin and "
+            "normalized spaces"),
+        ("merge", "cflsPp", "sample the input space"),
+        ("model", "fgilOops", "model the input space with a learned model"),
+        ("plot", "cflMmops", "plot the samples and predictions"),
+        ("predict", "flmO4", "predict performance values in output space"),
+        ("search", "flMmSo", "search optimal values in the output space"),
+        ("sample", "fgilnOo", "sample the input space"),
+    ):
         parsers[cmd] = command.add_parser(cmd, help=hlp)
         for f in flgs:
             v = _CommonOpts["1" if f.isdigit() else f]
@@ -159,13 +167,14 @@ def parse(argv: [str]):
                 v["k"]["choices"] = list(range(int(f)))
             parsers[cmd].add_argument(v["l"], **v["k"])
 
-    parsers["convert"].add_argument("-d", "--direction",
-        choices=("orig2norm", "norm2orig"))
+    parsers["convert"].add_argument(
+        "-d", "--direction", choices=("orig2norm", "norm2orig")
+    )
 
     return main.parse_args(args=argv[1:])
 
 
-def main(argv: [str]):
+def main(argv: [str]) -> int:
     args = parse(argv)
     if args.command is None:
         print("GKO or Generative Kernel Optimization.\n\n"
